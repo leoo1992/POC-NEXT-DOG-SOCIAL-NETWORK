2a2e6df50820bb784d18b709e3996a58e2002851
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-08-06 22:12:23 -0300

    complete style and code of form login

--- src/components/Login/login-form.tsx
+++ src/components/Login/login-form.tsx
@@ -1,24 +1,13 @@
 "use client";
 
 import login from "@/actions/login";
-import { useFormStatus, useFormState } from "react-dom";
-import Button from "@/components/UX/Button";
+import { useFormState } from "react-dom";
 import Input from "@/components/UX/Input";
 import ErrorMessage from "../Helper/error-message";
-
-function FormButton() {
-  const { pending } = useFormStatus();
-  
-  return (
-    <>
-      {pending ? (
-        <Button disabled>Entrando...</Button>
-      ) : (
-        <Button>Entrar</Button>
-      )}
-    </>
-  );
-}
+import { useEffect } from "react";
+import FormButton from "@/components/Login/form-button";
+import Link from "next/link";
+import styles from "./login-form.module.css";
 
 export default function LoginForm() {
   const [state, action] = useFormState(login, {
@@ -27,22 +16,40 @@ export default function LoginForm() {
     data: null,
   });
 
+  useEffect(() => {
+    if (state.ok) {
+      window.location.href = "/conta";
+    }
+  }, [state.ok]);
+
   return (
-    <form action={action}>
-      <Input
-        type="text"
-        label="Usuário"
-        name="username"
-        placeholder="Usuário"
-      />
-      <Input
-        type="password"
-        label="Senha"
-        name="password"
-        placeholder="Senha"
-      />
-      <ErrorMessage error={state.error} />
-      <FormButton />
-    </form>
+    <>
+      <form action={action}>
+        <Input
+          type="text"
+          label="Usuário"
+          name="username"
+          placeholder="Usuário"
+        />
+        <Input
+          type="password"
+          label="Senha"
+          name="password"
+          placeholder="Senha"
+        />
+        <ErrorMessage error={state.error} />
+        <FormButton text="Entrar" loading="Entrando..." />
+      </form>
+      <Link className={styles.perdeu} href="/login/esqueceu">
+        Esqueceu sua senha ?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className="button" href="/login/criar">
+          Cadastro
+        </Link>
+      </div>
+    </>
   );
 }
