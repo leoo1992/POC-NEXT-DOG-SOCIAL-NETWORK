ced0c786dde365b64672e1d461c95d6e19490023
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-08-07 22:32:34 -0300

    get user token and revalidate

--- src/components/UX/Header/index.tsx
+++ src/components/UX/Header/index.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import styles from './header.module.css';
 import Image from 'next/image';
+import userGet from '@/actions/user-get';
 
 export default async function Header() {
-  const user = false;
+  const {data} = await userGet();
 
   return (
     <header className={styles.header}>
@@ -17,9 +18,9 @@ export default async function Header() {
             priority
           />
         </Link>
-        {user ? (
-          <Link className={styles.login} href='/conta'>
-            Dogs
+        {data ? (
+          < Link className={styles.login} href='/conta'>
+            {data.username}
           </Link>
         ) : (
           <Link className={styles.login} href='/login'>
