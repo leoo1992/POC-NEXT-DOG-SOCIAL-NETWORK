c7c22978fe4a42f0fcb9a57b7ead0c1a96d61729
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-08-14 10:59:04 -0300

    init profile page

---
+++ src/actions/photo-get.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { PHOTO_GET } from "@/functions/api";
+import apiError from "@/functions/api-error";
+import { Photo } from "./photos-get";
+
+export type Comment = {
+    comment_ID: string;
+    comment_post_ID: string;
+    comment_author: string;
+    comment_content: string;
+}
+
+export type PhotoData = {
+  photo: Photo;
+  comment: Comment[];
+};
+export default async function photoGet(id: string) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+  try {
+    const { url } = PHOTO_GET(id),
+      response = await fetch(url, {
+        next: { revalidate: 60, tags: ["photos", "comment"] },
+      });
+
+    if (!response.ok) {
+      throw new Error("Erro ao pegar foto pelo id.");
+    }
+
+    const data = (await response.json()) as PhotoData;
+
+    return { data, ok: true, error: "" };
+  } catch (error) {
+    return apiError(error);
+  }
+}
--- src/app/foto/[id]/page.tsx
+++ src/app/foto/[id]/page.tsx
@@ -1,7 +1,14 @@
-export default async function FotoIdPage({params}:Readonly<{params:{id: number}}>) {
+import photoGet from "@/actions/photo-get";
+
+export default async function FotoIdPage({
+  params,
+}: Readonly<{ params: { id: string } }>) {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return null;
   return (
-    <section>
-      <h1 className="title">{params.id}</h1>
+    <section className="container mainContainer">
+      <h1 className="title">{data.photo?.title}</h1>
     </section>
   );
 }
